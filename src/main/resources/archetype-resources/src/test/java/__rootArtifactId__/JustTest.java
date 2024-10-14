b60ea8c64563db094d781b96ce55fde70a3e08f9
lixicheng <lixicheng@everfavour.cn>
2024-10-14 09:49:17 +0800

    fix: 修复测试类报错

--- src/main/resources/archetype-resources/src/test/java/__rootArtifactId__/JustTest.java
+++ src/main/resources/archetype-resources/src/test/java/__rootArtifactId__/JustTest.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.${artifactId}.test;
+package ${package}.${artifactId};
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
