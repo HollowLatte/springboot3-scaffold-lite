45093b1cb0cf16e7bbd32b66eaafc86ea18d1aa9
hollowlatte <hollowlatte@outlook.com>
2025-08-02 10:04:46 +0800

    refactor: 调整package-info

--- src/main/resources/archetype-resources/src/main/java/infrastructure/package-info.java
+++ src/main/resources/archetype-resources/src/main/java/infrastructure/package-info.java
@@ -3,5 +3,5 @@
 #set( $symbol_escape = '\' )
 /**
  * 基础设施层。负责所有技术细节的实现。比如，它会实现 domain 层定义的 Gateway 接口，通过 MyBatis、RPC 客户端等具体技术与数据库或外部服务交互。
- * */
+ */
 package ${package}.domain;
---
+++ src/main/resources/archetype-resources/src/main/java/types/package-info.java
@@ -0,0 +1,7 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * 通用类型层。存放通用基础类型、DTO、枚举、值对象
+ */
+package ${package}.client;
